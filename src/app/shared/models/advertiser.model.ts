ace41be52049b57e9d6a967739e1079a31774d7d
bdf2ch <bdf2ch@gmail.com>
2018-04-11 19:43:46 +0300

    random markers added

--- src/app/shared/models/advertiser.model.ts
+++ src/app/shared/models/advertiser.model.ts
@@ -1,12 +1,17 @@
-import { User } from './user.model';
 import { IAdvertiser } from '../interfaces/advertiser.interface';
 
 
-export class Advertiser extends User {
+export class Advertiser {
+  title: string;
   industry: string;
+  address: string;
+  phones: string[];
+  photos: string[];
 
   constructor(config?: IAdvertiser) {
-    super(config);
     this.industry = config ? config.industry : '';
+    this.address = '1355 Market St, Suite 900\n San Francisco, CA 94103';
+    this.phones = ['(123)456-789'];
+    this.photos = [];
   }
 }
